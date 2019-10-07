2e871b22917ee16d105d87ac6828dc5e92362087
Gustavo <gustavoabatistela@gmail.com>
2019-10-07 14:05:54 -0300

    Added example to test development

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 from neopz import *
-vec = PZVecDouble(2, 3)
+vec = TPZVecDouble(2, 3)
 vec.Resize(2)
 vec_item = vec[1]
 print(vec_item)
-mat = PZMatrix(2, 2, 3.)
+mat = TPZMatrix(2, 2, 3.)
 mat_item = mat.GetVal(1, 1)
 print(mat_item)
-print(PZQuad.SideDimension(9))
+print(TPZQuadrilateral.NSideNodes(7))
